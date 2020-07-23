1613566ebd81a04a099e9d0036bb4acc25aaecfa
ajaidan0 <ajaidan0@gmail.com>
2020-07-23 14:07:12 -0400

    add mavalry 2.0.1-b7

--- repo.sh
+++ repo.sh
@@ -5,6 +5,6 @@ rm -rf $WEBSITE/Packages
 rm -rf $WEBSITE/Packages.bz2
 
 echo building package list
-dpkg-scanpackages -m $WEBSITE/debs > $WEBSITE/Packages
+dpkg-scanpackages -m ./debs > ./Packages
 
-bzip2 -5fkv $WEBSITE/Packages > $WEBSITE/Packages.bz2
+bzip2 -5fkv ./Packages > ./Packages.bz2
